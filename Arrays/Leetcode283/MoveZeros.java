3305fe2ff0830e7c7a941314ad687d1770c39a9e
ravanrag <ravanrag@outlook.com>
2024-08-05 00:31:34 +0530

    move zeros

---
+++ Arrays/Leetcode283/MoveZeros.java
@@ -0,0 +1,19 @@
+package Arrays.Leetcode283;
+
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+
+        for(int i=0; i<nums.length-1;i++){
+            int j=i+1;
+            while (nums[i]==0 && j<nums.length ){
+                swap(nums, i,j);
+                j++;
+            }
+        }
+    }
+    public void swap(int[] nums, int a, int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
+}
