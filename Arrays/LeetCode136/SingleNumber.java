c4565bf8574f5aaf571319e7b1792e5eb6c74654
ravanrag <ravanrag@outlook.com>
2024-08-07 00:42:00 +0530

    singlenumber

---
+++ Arrays/LeetCode136/SingleNumber.java
@@ -0,0 +1,11 @@
+package Arrays.LeetCode136;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int a : nums){
+            ans=ans^a;
+        }
+        return ans;
+    }
+}
