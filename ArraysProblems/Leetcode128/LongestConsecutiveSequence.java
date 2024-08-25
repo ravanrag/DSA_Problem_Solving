4681483b58bc659a352ea6f4d767d9224e4e84d7
ravanrag <ravanrag@outlook.com>
2024-08-25 23:56:40 +0530

    8/10/24

--- ArraysProblems/Leetcode128/LongestConsecutiveSequence.java
+++ ArraysProblems/Leetcode128/LongestConsecutiveSequence.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LongestConsecutiveSequence {
-    public int longestConsecutive(int[] nums) {
+    public int longestConsecutiveSequence(int[] nums) {
         if(nums.length==0)
             return 0;
         Set<Integer> set = new HashSet<>();
