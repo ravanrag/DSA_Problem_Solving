9fbafb975f209f44de51fdccf6140a248f141e07
ravan <ravanrag@outlook.com>
2025-02-14 23:56:25 +0530

    Leetcode 16 and 3174

--- LeetCodeProblems/LeetCode169/MajorityElement.java
+++ LeetCodeProblems/LeetCode169/MajorityElement.java
@@ -15,4 +15,22 @@ public class MajorityElement {
         }
         return majorityElement;
     }
+    public int majorityElement2(int[] nums) {
+        int el=nums[0];
+        int count=1;
+        for(int i=1; i<nums.length;i++){
+            if(nums[i]==el){
+                count++;
+            }
+            else if(count==0){
+                el=nums[i];
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return el;
+    }
+
 }
