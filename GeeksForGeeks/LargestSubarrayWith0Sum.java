81c8dca6bf66838e3df0b962836faa91cbda6463
ravan <ravanrag@outlook.com>
2025-02-15 02:30:43 +0530

    Leetcode 16 and 3174

---
+++ GeeksForGeeks/LargestSubarrayWith0Sum.java
@@ -0,0 +1,26 @@
+package GeeksForGeeks;
+
+import java.util.HashMap;
+//https://www.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
+public class LargestSubarrayWith0Sum {
+    int maxLen(int arr[]) {
+        // code here
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int maxlen=0;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+            if(sum==0){
+                maxlen=Math.max(maxlen,i+1);
+            }
+            else if(hm.containsKey(sum)){
+                int pos = hm.get(sum);
+                maxlen=Math.max(maxlen,i-pos);
+            }
+            else{
+                hm.put(sum,i);
+            }
+        }
+        return maxlen;
+    }
+}
