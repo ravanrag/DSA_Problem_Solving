f086f04261fe6f9bc3f763a7c6882528ccc8909c
ravan <ravanrag@outlook.com>
2025-05-03 02:33:00 +0530

    new

---
+++ CodingNinjas/SubarraysWithZeroSum.java
@@ -0,0 +1,23 @@
+package CodingNinjas;
+
+import java.util.HashMap;
+
+public class SubarraysWithZeroSum {
+    public static int countSubarrays(int n, int[] arr) {
+        // Write your code here.
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        hm.put(0,1);
+        int count=0;
+        int prefixsum=0;
+        for(int i: arr){
+            prefixsum+=i;
+            if ( hm.containsKey(prefixsum)){
+                count+=hm.get(prefixsum);
+                hm.put(prefixsum,count+1);
+            }
+            else
+                hm.put(prefixsum,1);
+        }
+        return count;
+    }
+}
